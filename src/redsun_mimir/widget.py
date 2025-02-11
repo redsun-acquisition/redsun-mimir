22dfb1213c8ea4836267f8f052de3ec4c798db8c
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-02-11 16:37:21 +0100

    Use the virtual bus to retrieve motor configurations

--- src/redsun_mimir/widget.py
+++ src/redsun_mimir/widget.py
@@ -8,6 +8,8 @@ from sunflare.virtual import Signal
 if TYPE_CHECKING:
     from typing import Any
 
+    from bluesky.protocols import Reading
+    from event_model.documents.event_descriptor import DataKey
     from sunflare.config import RedSunSessionInfo
     from sunflare.virtual import VirtualBus
 
@@ -32,6 +34,7 @@ class StageWidget(BaseQtWidget):
     """
 
     sigMotorMove = Signal(str, float)
+    sigGetDescription = Signal()
 
     def __init__(
         self,
@@ -43,18 +46,49 @@ class StageWidget(BaseQtWidget):
         super().__init__(*args, **kwargs)
         self._config = config
         self._virtual_bus = virtual_bus
+        self._description: dict[str, dict[str, DataKey]] = {}
+        self._configuration: dict[str, dict[str, Reading]] = {}
 
     def registration_phase(self) -> None:
         """Register your signals to the virtual bus."""
         self._virtual_bus.register_signals(self)
 
     def connection_phase(self) -> None:
-        """Connect your signals to the virtual bus."""
+        """Connect your signals to the virtual bus.
+
+        The controller layer will be already built when this method is called.
+        We use it to directly build the GUI by retrieving a configuration of
+        the currently allocated motors to create the proper widget layout.
+        """
         self._virtual_bus["StageController"]["sigNewPosition"].connect(
             self._update_position
         )
+        self._virtual_bus["StageController"]["sigMotorDescription"].connect(
+            self._update_description
+        )
+        self._virtual_bus["StageController"]["sigMotorConfiguration"].connect(
+            self._update_configuration
+        )
+        self.sigGetDescription.emit()
+        # we have the configuration,
+        # we can build the GUI now
+        self._build_gui()
 
-    def _update_position(self, stage: str, position: float) -> None:
-        """Update the stage position."""
+    def _update_position(self, motor: str, position: float) -> None:
+        """Update the motor position."""
         # TODO: implement
+        ...
+
+    def _update_description(self, description: dict[str, dict[str, DataKey]]) -> None:
+        """Update the motor description."""
+        self._description = description
+
+    def _update_configuration(
+        self, configuration: dict[str, dict[str, Reading]]
+    ) -> None:
+        """Update the motor configuration."""
+        self._configuration = configuration
+
+    def _build_gui(self) -> None:
+        """Build the GUI."""
         ...
