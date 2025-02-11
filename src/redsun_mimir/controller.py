75dae0b308220a6c09764852118e6534521546cb
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-02-11 15:54:34 +0100

    Use a daemon loop with a queue
    - a minimal execution engine focusing on moving motors to a specific position
    - should work with both absolute and relative movements but currently untested

--- src/redsun_mimir/controller.py
+++ src/redsun_mimir/controller.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
-from concurrent.futures import ThreadPoolExecutor
-from typing import TYPE_CHECKING, Any, Mapping
+from threading import Thread
+from typing import TYPE_CHECKING, Any, Mapping, Optional, Callable
+from queue import Queue
 
 from sunflare.log import Loggable
 from sunflare.virtual import Signal, VirtualBus
 
 from ._protocols import MotorProtocol
+
+from event_model.documents.event_descriptor import DataKey
+from bluesky.protocols import Reading
 
 if TYPE_CHECKING:
     from sunflare.model import ModelProtocol
@@ -17,10 +21,83 @@ if TYPE_CHECKING:
     from .config import StageControllerInfo
 
 
+class DaemonLoop(Thread):
+    """A subclass of ``threading.Thread`` which runs a persistent daemon loop.
+
+    The daemon will continously inspect a queue of tasks to execute.
+    Whenever a task is available (a new position is requested),
+    the daemon will execute it in the background.
+
+    Parameters
+    ----------
+    queue : Queue[Optional[tuple[str, float]]]
+        Queue of tasks to execute.
+    motors : dict[str, MotorProtocol]
+        Mapping of motor names to motor instances.
+    exception_cb : Callable[[str], None]
+        Callback to handle exceptions. This should be
+        mapped to the main controller ``exception`` method.
+
+    Attributes
+    ----------
+    sigNewPosition : Signal[str, float]
+        Signal emitted when a new position is set.
+        Forwards the response to the controller which will
+        emit the same signal to the widget.
+        - ``str``: motor name
+        - ``float``: new position
+
+    """
+
+    sigNewPosition = Signal(str, float)
+
+    def __init__(
+        self,
+        queue: Queue[Optional[tuple[str, float]]],
+        motors: dict[str, MotorProtocol],
+        exception_cb: Callable[[str], None],
+    ) -> None:
+        super().__init__(daemon=True)
+        self._queue = queue
+        self._running = False
+        self._motors = motors
+        self._exception_cb = exception_cb
+
+    def run(self) -> None:
+        """Run the daemon loop."""
+        self._running = True
+        while self._running:
+            # block until a task is available
+            task = self._queue.get()
+            if task is not None:
+                motor, position = task
+                self._do_move(self._motors[motor], position)
+                self._queue.task_done()
+            else:
+                # Stop the daemon
+                self._running = False
+
+    def _do_move(self, motor: MotorProtocol, position: float) -> None:
+        """Move a motor to a given position.
+
+        Wait on the status object to complete in a background thread.
+        """
+        s = motor.set(position)
+        try:
+            s.wait()
+        except Exception as e:
+            self._exception_cb(f"Failed to move {motor.name} to {position}: {e}")
+        else:
+            self.sigNewPosition.emit(motor, position)
+
+
 class StageController(Loggable):
     """Motor stage controller for RedSun Mimir.
 
-    The controller allows manual setting of stage positions.
+    The controller allows manual setting of stage positions;
+    communication with the user interface is done via
+    signals exchanged with the ``StageWidget`` accross
+    the virtual bus.
 
     Whenever a new position is requested from ``StageWidget``
     via the ``sigMotorMove`` signal, the controller will move the stage
@@ -45,10 +122,20 @@ class StageController(Loggable):
         Signal emitted when a new position is set.
         - ``str``: motor name
         - ``float``: new position
+    sigMotorDescription : ``Signal[str, dict[str, DataKey]]``
+        Signal emitted when the motor configuration is described.
+        - ``str``: motor name
+        - ``dict[str, DataKey]``: motor configuration description
+    sigMotorConfiguration : ``Signal[str, dict[str, Reading]]``
+        Signal emitted when the motor configuration is read.
+        - ``str``: motor name
+        - ``dict[str, Reading]``: motor configuration
 
     """
 
     sigNewPosition = Signal(str, float)
+    sigMotorDescription = Signal(str, dict[str, DataKey])
+    sigMotorConfiguration = Signal(str, dict[str, Reading])
 
     def __init__(
         self,
@@ -59,7 +146,7 @@ class StageController(Loggable):
         self._ctrl_info = ctrl_info
         self._virtual_bus = virtual_bus
         self._plans: list[partial[MsgGenerator[Any]]] = []
-        self._pool = ThreadPoolExecutor(thread_name_prefix="StageController")
+        self._queue: Queue[Optional[tuple[str, float]]] = Queue()
 
         self._motors = {
             name: model
@@ -67,21 +154,15 @@ class StageController(Loggable):
             if isinstance(model, MotorProtocol)
         }
 
-    def move(self, motor: str, position: float) -> None:
-        """Move a motor to a given position."""
-        self._pool.submit(self._do_move, self._motors[motor], position)
+        self._daemon = DaemonLoop(self._queue, self._motors, self.exception)
+        self._daemon.sigNewPosition.connect(self.sigNewPosition.emit)
 
-    def _do_move(self, motor: MotorProtocol, position: float) -> None:
+    def move(self, motor: str, position: float) -> None:
         """Move a motor to a given position.
 
-        Wait on the status object to complete in a background thread.
+        Sends a new position to the daemon queue.
         """
-        s = motor.set(position)
-        try:
-            s.wait()
-        except Exception as e:
-            self.exception(f"Failed to move {motor.name} to {position}: {e}")
-        self.sigNewPosition.emit(motor, position)
+        self._queue.put((motor, position))
 
     def shutdown(self) -> None:
         """Shutdown the controller.
