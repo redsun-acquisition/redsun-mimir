81ca5437ddfdfa0d5e1877e9b2a1c88356618d08
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-08-26 14:30:14 +0200

    chore: fix init

--- src/redsun_mimir/configurations/__init__.py
+++ src/redsun_mimir/configurations/__init__.py
@@ -1,11 +1,11 @@
-from ._acquisition_only import acquisition_widget
-from ._acquisition_only_uc2 import acquisition_widget_uc2
-from ._detector_only import detector_widget
-from ._detector_only_uc2 import detector_widget_uc2
-from ._light_only import light_widget
-from ._light_only_uc2 import light_widget_uc2
-from ._motor_only import stage_widget
-from ._motor_only_uc2 import stage_widget_uc2
+from ._acquisition import acquisition_widget
+from ._acquisition_uc2 import acquisition_widget_uc2
+from ._detector import detector_widget
+from ._detector_uc2 import detector_widget_uc2
+from ._light import light_widget
+from ._light_uc2 import light_widget_uc2
+from ._motor import stage_widget
+from ._motor_uc2 import stage_widget_uc2
 
 __all__ = [
     "acquisition_widget",
