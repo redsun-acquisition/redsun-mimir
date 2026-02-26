6706503686f568499c91336d75c349ccf658ac89
Jacopo Abramo <jacopo.abramo@gmail.com>
2026-02-26 11:59:47 +0100

    refactor: remove ConfigurationDict
    - never used

--- src/redsun_mimir/common/__init__.py
+++ src/redsun_mimir/common/__init__.py
@@ -7,10 +7,8 @@ from ._plan_spec import (
     create_plan_spec,
     resolve_arguments,
 )
-from ._types import ConfigurationDict
 
 __all__ = [
-    "ConfigurationDict",
     "ParamDescription",
     "ParamKind",
     "PlanSpec",
