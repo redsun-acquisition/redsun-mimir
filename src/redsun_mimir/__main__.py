9efab7600850ee3886b4e79ecfc55200d0562fe9
Jacopo Abramo <jacopo.abramo@gmail.com>
2026-03-02 10:07:13 +0100

    feat: switch camera configuration to pre-configured classes

--- src/redsun_mimir/__main__.py
+++ src/redsun_mimir/__main__.py
@@ -14,17 +14,14 @@ def main() -> None:
     parser = ArgumentParser(description="CLI for redsun-mimir examples")
     subparsers = parser.add_subparsers(dest="command")
     subparsers.add_parser("sim", help="Run the full simulation example")
+    subparsers.add_parser(
+        "uc2", help="Run UC2 microscope application with pre-shipped configuration."
+    )
     subparsers.add_parser("motor", help="Run the mock motor example")
-    subparsers.add_parser("motor-uc2", help="Run the UC2 motor example")
     subparsers.add_parser("light", help="Run the mock light example")
-    subparsers.add_parser("light-uc2", help="Run the UC2 light example")
     subparsers.add_parser(
         "acquisition",
         help="Run the example acquisition container",
-    )
-    subparsers.add_parser(
-        "acquisition-uc2",
-        help="Run the UC2 example acquisition container",
     )
 
     options = parser.parse_args(namespace=Options())
@@ -32,16 +29,10 @@ def main() -> None:
         configurations.run_simulation_container()
     elif options.command == "motor":
         configurations.run_stage_container()
-    elif options.command == "motor-uc2":
-        configurations.run_youseetoo_motor_container()
     elif options.command == "light":
         configurations.run_light_container()
-    elif options.command == "light-uc2":
-        configurations.run_youseetoo_light_container()
     elif options.command == "acquisition":
         configurations.run_acquisition_container()
-    elif options.command == "acquisition-uc2":
-        configurations.run_youseetoo_acquisition_container()
     else:
         parser.print_help()
 
---
+++ src/redsun_mimir/configurations/_full_uc2.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+
+from redsun.containers import device, presenter, view
+from redsun.qt import QtAppContainer
+
+_CONFIG = Path(__file__).parent / "uc2_full_configuration.yaml"
+
+
+def run_full_uc2_container() -> None:
+    """Run the full UC2 microscope with pre-shipped configuration."""
+    # devices
+    from redsun_mimir.device import MockLightDevice, MockMotorDevice  # noqa: I001
+    from redsun_mimir.device.mmcore import MMCoreCameraDevice
+
+    # presenters
+    from redsun_mimir.presenter.storage import FileStoragePresenter
+    from redsun_mimir.presenter.acquisition import AcquisitionPresenter
+    from redsun_mimir.presenter.detector import DetectorPresenter
+    from redsun_mimir.presenter.light import LightPresenter
+    from redsun_mimir.presenter.median import MedianPresenter
+    from redsun_mimir.presenter.motor import MotorPresenter
+
+    # views
+    from redsun_mimir.view.acquisition import AcquisitionView
+    from redsun_mimir.view.detector import DetectorView
+    from redsun_mimir.view.image import ImageView
+    from redsun_mimir.view.light import LightView
+    from redsun_mimir.view.motor import MotorView
+    from redsun_mimir.view.storage import FileStorageView
+
+    logging.getLogger("redsun").setLevel(logging.DEBUG)
+
+    class MimirMicroscope(QtAppContainer, config=_CONFIG):
+        # devices
+        serial = device(MockLightDevice, from_config="serial")
+        iscat = device(MMCoreCameraDevice, from_config="camera")
+        stage = device(MockMotorDevice, from_config="stage")
+        laser = device(MockLightDevice, from_config="laser")
+
+        # presenters
+        storage_ctrl = presenter(FileStoragePresenter, from_config="storage_ctrl")
+        median_ctrl = presenter(MedianPresenter, from_config="median_ctrl")
+        det_ctrl = presenter(DetectorPresenter, from_config="det_ctrl")
+        acq_ctrl = presenter(AcquisitionPresenter, from_config="acq_ctrl")
+        light_ctrl = presenter(LightPresenter, from_config="light_ctrl")
+        motor_ctrl = presenter(MotorPresenter, from_config="motor_ctrl")
+
+        # views
+        acq_widget = view(AcquisitionView, from_config="acq_widget")
+        img_widget = view(ImageView, from_config="img_widget")
+        det_widget = view(DetectorView, from_config="det_widget")
+        light_widget = view(LightView, from_config="light_widget")
+        motor_widget = view(MotorView, from_config="motor_widget")
+        storage_widget = view(FileStorageView, from_config="storage_widget")
+
+    MimirMicroscope().run()
